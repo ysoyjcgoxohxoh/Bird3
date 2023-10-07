956de3b7ea1572873c9c6aa17f1f06578580cc1d
ysoyjcgoxohxoh <ysoyjcgoxohxoh@gmail.com>
2023-10-07 09:00:43 -0400

    Add .net Windows Forms application to unlock battery and control motor.

---
+++ BirdCAN/BirdCAN/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BirdCAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      this.cbCANAdapter = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.tbMotorSpeed = new System.Windows.Forms.TrackBar();
+      this.chkRearLight = new System.Windows.Forms.CheckBox();
+      this.btnConnect = new System.Windows.Forms.Button();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.label2 = new System.Windows.Forms.Label();
+      this.chkUnlockBattery = new System.Windows.Forms.CheckBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.cbChannel = new System.Windows.Forms.ComboBox();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      ((System.ComponentModel.ISupportInitialize)(this.tbMotorSpeed)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // cbCANAdapter
+      // 
+      this.cbCANAdapter.DisplayMember = "Path";
+      this.cbCANAdapter.FormattingEnabled = true;
+      this.cbCANAdapter.Location = new System.Drawing.Point(120, 16);
+      this.cbCANAdapter.Margin = new System.Windows.Forms.Padding(4);
+      this.cbCANAdapter.Name = "cbCANAdapter";
+      this.cbCANAdapter.Size = new System.Drawing.Size(243, 24);
+      this.cbCANAdapter.TabIndex = 1;
+      this.cbCANAdapter.SelectedIndexChanged += new System.EventHandler(this.cbCANAdapter_SelectedIndexChanged);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(16, 21);
+      this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(89, 16);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "CAN Adapter:";
+      // 
+      // tbMotorSpeed
+      // 
+      this.tbMotorSpeed.Location = new System.Drawing.Point(16, 135);
+      this.tbMotorSpeed.Margin = new System.Windows.Forms.Padding(4);
+      this.tbMotorSpeed.Maximum = 400;
+      this.tbMotorSpeed.Name = "tbMotorSpeed";
+      this.tbMotorSpeed.Size = new System.Drawing.Size(399, 56);
+      this.tbMotorSpeed.TabIndex = 7;
+      this.tbMotorSpeed.TickFrequency = 413;
+      this.tbMotorSpeed.Scroll += new System.EventHandler(this.tbMotorSpeed_Scroll);
+      // 
+      // chkRearLight
+      // 
+      this.chkRearLight.AutoSize = true;
+      this.chkRearLight.Location = new System.Drawing.Point(16, 89);
+      this.chkRearLight.Margin = new System.Windows.Forms.Padding(4);
+      this.chkRearLight.Name = "chkRearLight";
+      this.chkRearLight.Size = new System.Drawing.Size(90, 20);
+      this.chkRearLight.TabIndex = 5;
+      this.chkRearLight.Text = "Rear Light";
+      this.chkRearLight.UseVisualStyleBackColor = true;
+      this.chkRearLight.CheckedChanged += new System.EventHandler(this.chkRearLight_CheckedChanged);
+      // 
+      // btnConnect
+      // 
+      this.btnConnect.Location = new System.Drawing.Point(620, 15);
+      this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+      this.btnConnect.Name = "btnConnect";
+      this.btnConnect.Size = new System.Drawing.Size(100, 28);
+      this.btnConnect.TabIndex = 3;
+      this.btnConnect.Text = "Connect";
+      this.btnConnect.UseVisualStyleBackColor = true;
+      this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.AllowUserToAddRows = false;
+      this.dataGridView1.AllowUserToDeleteRows = false;
+      this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+      this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(16, 235);
+      this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersVisible = false;
+      this.dataGridView1.RowHeadersWidth = 51;
+      this.dataGridView1.Size = new System.Drawing.Size(704, 288);
+      this.dataGridView1.TabIndex = 9;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(20, 215);
+      this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(120, 16);
+      this.label2.TabIndex = 8;
+      this.label2.Text = "Recent Messages:";
+      // 
+      // chkUnlockBattery
+      // 
+      this.chkUnlockBattery.AutoSize = true;
+      this.chkUnlockBattery.Location = new System.Drawing.Point(16, 60);
+      this.chkUnlockBattery.Margin = new System.Windows.Forms.Padding(4);
+      this.chkUnlockBattery.Name = "chkUnlockBattery";
+      this.chkUnlockBattery.Size = new System.Drawing.Size(116, 20);
+      this.chkUnlockBattery.TabIndex = 4;
+      this.chkUnlockBattery.Text = "Unlock Battery";
+      this.chkUnlockBattery.UseVisualStyleBackColor = true;
+      this.chkUnlockBattery.CheckedChanged += new System.EventHandler(this.chkRearLight_CheckedChanged);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(20, 116);
+      this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(91, 16);
+      this.label3.TabIndex = 6;
+      this.label3.Text = "Motor Torque:";
+      // 
+      // cbChannel
+      // 
+      this.cbChannel.DisplayMember = "Index";
+      this.cbChannel.FormattingEnabled = true;
+      this.cbChannel.Location = new System.Drawing.Point(372, 16);
+      this.cbChannel.Margin = new System.Windows.Forms.Padding(4);
+      this.cbChannel.Name = "cbChannel";
+      this.cbChannel.Size = new System.Drawing.Size(239, 24);
+      this.cbChannel.TabIndex = 2;
+      // 
+      // timer1
+      // 
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(736, 538);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.chkUnlockBattery);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.btnConnect);
+      this.Controls.Add(this.chkRearLight);
+      this.Controls.Add(this.tbMotorSpeed);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.cbChannel);
+      this.Controls.Add(this.cbCANAdapter);
+      this.Margin = new System.Windows.Forms.Padding(4);
+      this.Name = "Form1";
+      this.Text = "Bird 3";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.tbMotorSpeed)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCANAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbMotorSpeed;
+        private System.Windows.Forms.CheckBox chkRearLight;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkUnlockBattery;
+        private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.ComboBox cbChannel;
+    private System.Windows.Forms.Timer timer1;
+  }
+}
+
---
+++ BirdCAN/BirdCAN/Form1.cs
@@ -0,0 +1,348 @@
+﻿using Candle;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace BirdCAN
+{
+  public partial class Form1 : Form
+  {
+    Device selectedDevice;
+    Channel ConnectedChannel;
+    CancellationTokenSource cancel;
+    Dictionary<uint, Tuple<DateTime, Frame>> RecentMessages = new Dictionary<uint, Tuple<DateTime, Frame>>();
+    DataTable previewTable = new DataTable();
+    Dictionary<uint, DataRow> rowFinder = new Dictionary<uint, DataRow>();
+    DataColumn colTimeStamp;
+    DataColumn colID;
+    DataColumn colInterval;
+    DataColumn colData;
+    DataColumn colIntent;
+    int CurrentSpeed = 0;
+    bool UnlockBattery = false;
+    bool RearLightOn = false;
+    Dictionary<uint, Tuple<int, string>> names = new Dictionary<uint, Tuple<int, string>>();
+    public Form1()
+    {
+      InitializeComponent();
+      this.colTimeStamp = previewTable.Columns.Add("TimeStamp");
+      this.colID = previewTable.Columns.Add("ID");
+      this.colInterval = previewTable.Columns.Add("Interval");
+      this.colData = previewTable.Columns.Add("Data");
+      this.colIntent = previewTable.Columns.Add("Intent");
+      colTimeStamp.DataType = colID.DataType = colData.DataType = colInterval.DataType = colInterval.DataType = typeof(string);
+
+      this.dataGridView1.DataSource = previewTable;
+      names[frmRearLightOff.Identifier] = new Tuple<int, string>(50, "Rear Light");
+      names[frmBatteryLocked.Identifier] = new Tuple<int, string>(200, "Battery Output Enable");
+      names[BatteryUnlockCodes[0].Identifier] = new Tuple<int, string>(1000, "Controller Uptime/Battery Keepalive");
+
+      names[0x153] = new Tuple<int, string>(2, "Motor Control");
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      var devices = Candle.Device.ListDevices().ToList<object>();
+      devices.Insert(0, new { Path = "(None)" });
+      cbCANAdapter.DataSource = devices;
+      chkRearLight.Enabled = chkUnlockBattery.Enabled = tbMotorSpeed.Enabled = false;
+    }
+
+    private void btnConnect_Click(object sender, EventArgs e)
+    {
+      if (cbChannel.SelectedItem is Channel)
+      {
+        if (ConnectedChannel == null)
+        {
+          try
+          {
+            cbChannel.Enabled = cbCANAdapter.Enabled = false;
+            chkRearLight.Enabled = chkUnlockBattery.Enabled = tbMotorSpeed.Enabled = true;
+            tbMotorSpeed.Value = 0;
+            batteryUnlockIndex = 0;
+            ConnectedChannel = cbChannel.SelectedItem as Channel;
+            ConnectedChannel.Start(500000);
+            cancel = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(TimerLoop);
+            timer1.Enabled = true;
+          }
+          catch
+          {
+            cbChannel.Enabled = cbCANAdapter.Enabled = true;
+            chkRearLight.Enabled = chkUnlockBattery.Enabled = tbMotorSpeed.Enabled = false;
+          }
+        }
+        else
+        {
+          cancel.Cancel();
+          Thread.Sleep(100); // give time for timer thread to finish
+          // Already connected, and button is now `Disconnect` button
+          ConnectedChannel.Stop();
+          ConnectedChannel.Receive();
+          ConnectedChannel = null;
+          cbChannel.Enabled = cbCANAdapter.Enabled = true;
+          chkRearLight.Enabled = chkUnlockBattery.Enabled = tbMotorSpeed.Enabled = false;
+          timer1.Enabled = false;
+        }
+      }
+      else
+      {
+        MessageBox.Show("Please select a device and channel");
+      }
+    }
+
+    private void cbCANAdapter_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      var device = cbCANAdapter.SelectedItem as Device;
+      if (selectedDevice != null && selectedDevice != device)
+        selectedDevice.Close();
+      selectedDevice = device;
+      if (device != null)
+      {
+        device.Open();
+        cbChannel.DataSource = device.Channels.Select(x => x.Value).ToList();
+      }
+      else
+      {
+        cbChannel.DataSource = null;
+      }
+    }
+
+    void TimerLoop(object state)
+    {
+      if (ConnectedChannel == null)
+      {
+        return;
+      }
+      var timerCounter = 0;
+      var timerIntervalMs = 2;
+      int timerCounterMax = 5000;
+      PrecisionRepeatActionOnInterval(new TimeSpan(timerIntervalMs * TimeSpan.TicksPerMillisecond), cancel.Token, () =>
+      {
+        if (cancel.Token.IsCancellationRequested)
+        {
+          return;
+        }
+
+
+        // no longer in startup. Time to start sending motor speed
+        var speed = (uint)CurrentSpeed;
+        var bytes = BitConverter.GetBytes(speed);
+        var x = bytes[1];
+        if (speed == 0)
+        {
+          bytes[3] = 1;
+        }
+        //bytes[1] = bytes[0];
+        //bytes[0] = x;
+        var frame = new Frame { Identifier = 0x153, Data = bytes };
+        RecentMessages[frame.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, frame);
+        ConnectedChannel.Send(frame);
+        if (timerCounter % 200 == 0)
+        {
+          if (UnlockBattery)
+          {
+            RecentMessages[frmBatteryUnlocked.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, frmBatteryUnlocked);
+            ConnectedChannel.Send(frmBatteryUnlocked);
+          }
+          else
+          {
+            RecentMessages[frmBatteryLocked.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, frmBatteryLocked);
+            ConnectedChannel.Send(frmBatteryLocked);
+          }
+        }
+
+
+        if (timerCounter % 50 == 0)
+        {
+          if (RearLightOn)
+          {
+            RecentMessages[frmRearLightOn.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, frmRearLightOn);
+            ConnectedChannel.Send(frmRearLightOn);
+          }
+          else
+          {
+            RecentMessages[frmRearLightOff.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, frmRearLightOff);
+            ConnectedChannel.Send(frmRearLightOff);
+          }
+        }
+
+
+        if ((timerCounter + 1000) % 1000 == 0)
+        { // Every one second
+          var batteryMessageFrame = BatteryUnlockCodes[batteryUnlockIndex];
+          RecentMessages[batteryMessageFrame.Identifier] = new Tuple<DateTime, Frame>(DateTime.Now, batteryMessageFrame);
+          ConnectedChannel.Send(batteryMessageFrame);
+
+          batteryUnlockIndex++;
+          if (batteryUnlockIndex >= BatteryUnlockCodes.Count)
+            batteryUnlockIndex = firstBatteryUnlockLoopIndex;
+        }
+        timerCounter += timerIntervalMs;
+        if (timerCounter == timerCounterMax)
+          timerCounter = 0;
+
+      });
+    }
+    Frame frmBatteryUnlocked = new Frame { Identifier = 0x140, Data = new byte[] { 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
+    Frame frmBatteryLocked = new Frame { Identifier = 0x140, Data = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
+    Frame frmRearLightOn = new Frame { Identifier = 0x500, Data = new byte[] { 0x01, 0x00, 0x00 } };
+    Frame frmRearLightOff = new Frame { Identifier = 0x500, Data = new byte[] { 0x00, 0x00, 0x00 } };
+
+    int batteryUnlockIndex = 0;
+    int firstBatteryUnlockLoopIndex = 5;
+    List<Frame> BatteryUnlockCodes = new List<Frame>(new[]
+    {
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x01, 0x00, 0xdd, 0x00, 0xcc, 0x35, 0xe0, 0x26}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x02, 0x00, 0xdd, 0x43, 0xb3, 0x02, 0x40, 0x8b}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x03, 0x00, 0xdd, 0x43, 0x05, 0x46, 0x0f, 0x6c}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x04, 0x00, 0xdd, 0x43, 0x34, 0xe5, 0x5e, 0x8d}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x05, 0x00, 0xdd, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x06, 0x00, 0x83, 0x43, 0xb1, 0x49, 0xc5, 0xa3}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x07, 0x00, 0x5f, 0x43, 0x66, 0xc8, 0x35, 0xf4}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x08, 0x00, 0x59, 0x43, 0xe1, 0xfd, 0x31, 0xfa}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x09, 0x00, 0xc4, 0x43, 0x76, 0xc7, 0x18, 0xe0}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0a, 0x00, 0x64, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0b, 0x00, 0x13, 0x43, 0x01, 0x2a, 0x41, 0xa7}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0c, 0x00, 0x44, 0x43, 0x14, 0x5b, 0x5e, 0x3a}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0d, 0x00, 0x07, 0x43, 0xba, 0x86, 0xf7, 0x96}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0e, 0x00, 0xee, 0x43, 0x11, 0x3c, 0x37, 0x6a}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x0f, 0x00, 0xd0, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x10, 0x00, 0xc5, 0x43, 0xd6, 0x30, 0x20, 0x92}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x11, 0x00, 0xb6, 0x43, 0x4c, 0xa9, 0xf2, 0x89}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x12, 0x00, 0x70, 0x43, 0xf2, 0x1d, 0x15, 0x2a}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x13, 0x00, 0x23, 0x43, 0x05, 0x87, 0xa9, 0x46}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x14, 0x00, 0x71, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x15, 0x00, 0x9c, 0x43, 0x79, 0x0a, 0x38, 0x91}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x16, 0x00, 0xb6, 0x43, 0xa4, 0xb7, 0xf0, 0x76}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x17, 0x00, 0x80, 0x43, 0xe4, 0xaf, 0x27, 0xfb}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x18, 0x00, 0x75, 0x43, 0x5c, 0x80, 0x47, 0xa3}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x19, 0x00, 0x0f, 0x43, 0x03, 0x57, 0x43, 0x41}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1a, 0x00, 0x2f, 0x43, 0x8a, 0xa3, 0x4c, 0x6c}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1b, 0x00, 0x21, 0x43, 0x36, 0xe7, 0x21, 0xc3}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1c, 0x00, 0x0f, 0x43, 0x79, 0xc0, 0xab, 0xba}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1d, 0x00, 0x29, 0x43, 0xb6, 0x44, 0x3b, 0xce}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1e, 0x00, 0x6a, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x1f, 0x00, 0xfc, 0x43, 0x66, 0x48, 0x45, 0xc7}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x20, 0x00, 0xb5, 0x43, 0xc9, 0xfa, 0xdd, 0x9f}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x21, 0x00, 0xbf, 0x43, 0x68, 0x9b, 0x32, 0x4c}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x22, 0x00, 0x9b, 0x43, 0xeb, 0x43, 0xdf, 0x90}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x23, 0x00, 0xf7, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x24, 0x00, 0x4c, 0x43, 0x96, 0x3c, 0x96, 0x04}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x25, 0x00, 0xe2, 0x43, 0x48, 0x2f, 0x34, 0xb5}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x26, 0x00, 0xdc, 0x43, 0x27, 0xfc, 0x06, 0xb0}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x27, 0x00, 0xf5, 0x43, 0xd7, 0x21, 0x01, 0x97}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x28, 0x00, 0xd7, 0x43, 0x28, 0x00, 0x2a, 0x00}},
+      new Frame { Identifier = 0x742,  Data = new byte[]{0x29, 0x00, 0x39, 0x43, 0xcd, 0x3c, 0x36, 0x2c } }
+    });
+
+    public static void PrecisionRepeatActionOnInterval(TimeSpan interval, CancellationToken? ct, Action action)
+    {
+      long stage1Delayms = 20;
+      long stage2Delayms = 5 * TimeSpan.TicksPerMillisecond;
+      bool USE_SLEEP0 = true;
+
+      DateTime target = DateTime.Now + new TimeSpan(0, 0, 0, 0, (int)stage1Delayms + 2);
+      bool warmup = true;
+      while (ct == null || !ct.Value.IsCancellationRequested)
+      {
+        // Getting closer to 'target' - Lets do the less precise but least cpu intesive wait
+        var timeLeft = target - DateTime.Now;
+        if (timeLeft.TotalMilliseconds >= stage1Delayms)
+        {
+          try
+          {
+            Thread.Sleep((int)(timeLeft.TotalMilliseconds - stage1Delayms));
+          }
+          catch (TaskCanceledException) when (ct != null)
+          {
+            return;
+          }
+        }
+
+        // Getting closer to 'target' - Lets do the semi-precise but mild cpu intensive wait - Thread.Sleep(0)
+        // Note: Thread.Sleep(0) is removed below because it is sometimes looked down on and also said not good to mix 'Thread.Sleep(0)' with Tasks.
+        //       However, Thread.Sleep(0) does have a quicker and more reliable turn around time then Task.Yield() so to 
+        //       make up for this a longer (and more expensive) Thread.SpinWait(1) would be needed.
+        if (USE_SLEEP0)
+        {
+          while (DateTime.Now < target - new TimeSpan(stage2Delayms / 8))
+          {
+            Thread.Sleep(0);
+          }
+        }
+
+        // Extreamlly close to 'target' - Lets do the most precise but very cpu/battery intesive 
+        while (DateTime.Now < target)
+        {
+          Thread.SpinWait(64);
+        }
+
+        if (!warmup)
+        {
+          action();
+          target += interval;
+        }
+        else
+        {
+          long start1 = DateTime.Now.Ticks + ((long)interval.TotalMilliseconds * TimeSpan.TicksPerMillisecond);
+          long alignVal = start1 - (start1 % ((long)interval.TotalMilliseconds * TimeSpan.TicksPerMillisecond));
+          target = new DateTime(alignVal);
+          warmup = false;
+        }
+      }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      var messages = RecentMessages.Values.ToList();
+      foreach (var message in messages)
+      {
+        DataRow dataRow;
+        if (rowFinder.ContainsKey(message.Item2.Identifier))
+        {
+          dataRow = rowFinder[message.Item2.Identifier];
+        }
+        else
+        {
+          dataRow = this.previewTable.NewRow();
+          dataRow[colID] = "0x" + Convert.ToString(message.Item2.Identifier, 16);
+          if (names.ContainsKey(message.Item2.Identifier))
+          {
+            dataRow[colIntent] = names[message.Item2.Identifier].Item2;
+            dataRow[colInterval] = names[message.Item2.Identifier].Item1.ToString() + " ms";
+          }
+          rowFinder[message.Item2.Identifier] = dataRow;
+          this.previewTable.Rows.Add(dataRow);
+        }
+        dataRow[colTimeStamp] = message.Item1.ToString("hh:mm:ss.fff");
+        dataRow[colData] = string.Join(", ", message.Item2.Data.Select(x => "0x" + Convert.ToString(x, 16).PadLeft(2, '0')));
+      }
+    }
+
+    private void tbMotorSpeed_Scroll(object sender, EventArgs e)
+    {
+      CurrentSpeed = tbMotorSpeed.Value;
+    }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+      btnConnect_Click(null, null);
+      selectedDevice?.Close();
+    }
+
+    private void chkRearLight_CheckedChanged(object sender, EventArgs e)
+    {
+      UnlockBattery = chkUnlockBattery.Checked;
+      RearLightOn = chkRearLight.Checked;
+    }
+  }
+}
